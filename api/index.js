1476a653101e9724fd91f3694efe199e5cd38f9f
sujeeth <sujeethkumar.j007@gmail.com>
2026-08-14 14:35:39 +0530

    await connectDB in vercel handler

--- api/index.js
+++ api/index.js
@@ -1,3 +1,11 @@
 const app = require('../server/app');
+const connectDB = require('../server/config/db');
 
-module.exports = app;
+module.exports = async (req, res) => {
+    try {
+        await connectDB();
+    } catch (err) {
+        console.error('Database connection failed:', err);
+    }
+    return app(req, res);
+};
